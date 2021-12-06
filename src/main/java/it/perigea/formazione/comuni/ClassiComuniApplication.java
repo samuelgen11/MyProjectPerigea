d5c00fc00583316c22ae7756010b5a48f63b3ca2
samuelgen11 <samuelgen11@gmail.com>
2021-12-06 22:25:26 +0100

    classi comuni

---
+++ src/main/java/it/perigea/formazione/comuni/ClassiComuniApplication.java
@@ -0,0 +1,13 @@
+package it.perigea.formazione.comuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassiComuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassiComuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/perigea/formazione/comuni/ClassiComuniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.perigea.formazione.comuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassiComuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
